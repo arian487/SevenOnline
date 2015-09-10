cea2dc533d9c533940df4d2bbbe2926f2eefa079
Arian Karbasi <akarbasi@akarbasi-macbookair.roam.corp.google.com>
2015-09-10 11:34:14 -0400

    Making some cleanup to wonder and constants

--- SevenOnline/constants.py
+++ SevenOnline/constants.py
@@ -1,11 +1,21 @@
-class constants(object)
-	STONE = 0
-	CLAY = 0
-	PAPER = 0
-	WOOD = 0
-	FABRIC = 0
-	ORE = 0
-	GLASS = 0
+class constants(object):
+  STONE = 0
+  CLAY = 1
+  PAPER = 2
+  WOOD = 3
+  FABRIC = 4
+  ORE = 5
+  GLASS = 6
+
+  WONDER_NAME = {
+  	0: 'Giza',
+  	1: 'Babylon',
+  	2: 'Ephesus',
+  	3: 'Olympia',
+  	4: 'Halicarnassus',
+  	5: 'Rhodes',
+  	6: 'Alexandria'
+  }
 	
 
 	
--- SevenOnline/player.py
+++ SevenOnline/player.py
@@ -1,6 +1,7 @@
 """Data for a Player."""
 
 import time
+import constants
 
 
 class Player(object):
@@ -26,10 +27,11 @@ class Player(object):
 
     self.ping()
     self.set_name('player.%s' % puid[:4])
-  def make_Wonder(num, side)
+
+  def MakeWonder(num, side)
     self.wonder = Wonder(num, side)
-	self.AddPermanentResources(self.wonder.resource)
-	return self.wonder
+    # Add a single resource of value 1 for a wonder
+	  self.AddPermanentResources(self.wonder.resource, 1)
 
   def set_name(self, namee):
     """modifies the users name."""
@@ -48,56 +50,26 @@ class Player(object):
     if resource code not in self.resources:
       self.resources[resource_code] = value
     else:
-        self.resources[resource_code] += value
+      self.resources[resource_code] += value
 
 """ this should be included in Player I think, not sure if the indentation is right might be in "AddPermanentResources" lol """		
-	class Wonder(object):
-		def __init__(self, num, side);
-			self.num = num
-			self.name = number_to_wonder(self.num) # name of wonder
-			self.side = side # side of wonder
-			self.resource = number_to_resource(self.num) #wonder's intrinsic resource
-			self.wonderCost = []
-			self.wonderCost = make_wonderCost(self.num, self.side) #array containing the wonders cost for each level
-			self.wonderLevel = 0 #wonder's current built level
+class Wonder(object):
+	def __init__(self, wonder_num, side);
+		self.wonder_num = wonder_num
+		self.name = constants.WONDERNAME[wonder_num] # name of wonder
+		self.side = side # side of wonder
+		self.resource = wonder_num #wonder's intrinsic resource
+		self.wonder_cost = []
+		self.wonder_cost = GetWonderCost(self.num, self.side) #array containing the wonders cost for each level
+		self.wonder_level = 0 #wonder's current built level
 			
-			
-			
-			def number_to_resource(argument):
-				switcher = {
-					0: constants.STONE,  
-					1: constants.CLAY,
-					2: constants.PAPER,
-					3: constants.WOOD,
-					4: constants.FABRIC,
-					5: constants.ORE,
-					6: constants.GLASS,
-					
-				}
-				return switcher.get(argument, "nothing")
-			
-			def number_to_wonder(argument):
-				switcher = {
-					0: "Giza",
-					1: "Babylon",
-					2: "Ephesus",
-					3: "Olympia",
-					4: "Halicarnassus",
-					5: "Rhodes",
-					6: "Alexandria".
-				}
-				return switcher.get(argument, "nothing")
 				
-			def make_wonderCost(num, side):
-				
-				"""need some way to import the costs, or else hard code, should have an array for each of the levels"""
-				
-				return 
+	def GetWonderCost(num, side):
+		"""need some way to import the costs, or else hard code, should have an array for each of the levels"""
+		return 
 			
-
-
-		def increment_wonderLevel(self):
-			self.wonderLevel += 1
+	def IncrementWonder(self):
+			self.wonder_level += 1
 
 
 	
