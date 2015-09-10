cee337c7c26ead4c7fb2781f5c5b286e81b7d733
Arian Karbasi <akarbasi@akarbasi-macbookair.roam.corp.google.com>
2015-09-10 11:44:25 -0400

    fixing constants.py

--- SevenOnline/constants.py
+++ SevenOnline/constants.py
@@ -1,21 +1,19 @@
-class constants(object):
-  STONE = 0
-  CLAY = 1
-  PAPER = 2
-  WOOD = 3
-  FABRIC = 4
-  ORE = 5
-  GLASS = 6
+# Resource codes for the resource types
+STONE = 0
+CLAY = 1
+PAPER = 2
+WOOD = 3
+FABRIC = 4
+ORE = 5
+GLASS = 6
 
-  WONDER_NAME = {
-  	0: 'Giza',
-  	1: 'Babylon',
-  	2: 'Ephesus',
-  	3: 'Olympia',
-  	4: 'Halicarnassus',
-  	5: 'Rhodes',
-  	6: 'Alexandria'
-  }
-	
-
-	
+# A mapping of the name
+WONDER_NAME = {
+  0: 'Giza',
+  1: 'Babylon',
+  2: 'Ephesus',
+  3: 'Olympia',
+  4: 'Halicarnassus',
+  5: 'Rhodes',
+  6: 'Alexandria'
+ }
